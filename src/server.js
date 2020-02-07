f3c11ff4edefa00825e7a1cbcf6eb12b887d00e1
karlgama <kaiquesilva49@hotmail.com>
2020-02-07 14:33:10 -0300

    Atualizando interface

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const app = express()
 
 app.use(express.json())
 
+let allowCrossDomain = (req,res, next) => {
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','*')
+    next()
+}
+
+app.use(allowCrossDomain)
+
 const auth = require('./routes/authRoutes')
 const series = require('./routes/seriesRoutes')
 
