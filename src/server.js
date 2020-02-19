0b36218fd574da225fbfb7383e99f11c25de8baf
karlgama <kaiquesilva49@hotmail.com>
2020-02-19 13:26:44 -0300

    att

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.use(express.json())
 let allowCrossDomain = (req,res, next) => {
     res.header('Access-Control-Allow-Origin','*')
     res.header('Access-Control-Allow-Headers','*')
+    res.header('Access-Control-Allow-Methods','*')
     next()
 }
 
